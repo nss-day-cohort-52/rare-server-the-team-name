4e4d2b41e061b5c70d9a11f05a94d3889a0acafe
Jubjub422 <ross.stovenour@gmail.com>
2022-01-25 10:37:37 -0600

    added models

---
+++ models/user.py
@@ -0,0 +1,15 @@
+import profile
+
+
+class User():
+    def __init__(self, id, first_name, last_name, email, bio, username, password, created_on, active, profile_image_url=""):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.bio = bio
+        self.username = username
+        self.password = password
+        self.created_on = created_on
+        self.active = active
+        self. profile_image_url = profile_image_url
