48b09244df51d0b484951fba399c4189742d67b2
jmason87 <joewebdev87@gmail.com>
2022-01-27 09:54:51 -0600

    able to delete a comment from a post

--- request_handler.py
+++ request_handler.py
@@ -3,9 +3,10 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import (create_category, create_tag, create_user, delete_post,
                    get_all_categories, get_all_posts, get_all_tags,
                    get_all_users, get_single_post, get_tags_by_label,
-                   login_user, get_single_user, get_all_comments)
+                   login_user, get_single_user, get_all_comments,delete_comment)
 
 from views import create_post, update_post, create_post,get_posts_by_category, get_certain_post_tags, create_post_tag
+
 
 class HandleRequests(BaseHTTPRequestHandler):
     """Handles the requests to this server"""
@@ -139,6 +140,8 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         if resource == "posts":
             delete_post(id)
+        if resource == "comments":
+            delete_comment(id)
 
         self.wfile.write("".encode())
 
