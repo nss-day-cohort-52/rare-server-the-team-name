d1ba2821e2af03087c357bd5a4018c9990f80797
Ashley Dickey <adickey93@gmail.com>
2022-01-27 09:34:23 -0600

    subscribing to a user

--- request_handler.py
+++ request_handler.py
@@ -6,7 +6,8 @@ from views import (create_category, create_post, create_post_tag,
                    get_all_categories, get_all_posts, get_all_tags,
                    get_all_users, get_certain_post_tags, get_posts_by_category,
                    get_single_post, get_single_user, get_tags_by_label,
-                   login_user, update_post, get_all_comments)
+                   login_user, update_post, get_all_comments, get_all_subscriptions)
+
 
 class HandleRequests(BaseHTTPRequestHandler):
     """Handles the requests to this server"""
@@ -77,6 +78,8 @@ class HandleRequests(BaseHTTPRequestHandler):
                     response = get_all_users()
             elif resource == "comments":
                 response = get_all_comments()
+            elif resource == "subscriptions":
+                response = get_all_subscriptions()
 
         elif len(parsed) == 3:
             (resource, key, value) = parsed
--- views/subscription_requests.py
+++ views/subscription_requests.py
@@ -2,6 +2,22 @@ import json
 import sqlite3
 from models import Subscription
 
+def get_all_subscriptions():
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+        *
+        FROM Subscriptions
+        """)
+        subscriptions = []
+        dataset = db_cursor.fetchall()
+    for row in dataset:
+        subscription = Subscription(row['id'], row['follower_id'], row['author_id'], row['created_on'])
+        subscriptions.append(subscription.__dict__)
+    return json.dumps(subscriptions)
+
 def create_subscription(new_subscription):
     with sqlite3.connect("./db.sqlite3") as conn:
         db_cursor = conn.cursor()
